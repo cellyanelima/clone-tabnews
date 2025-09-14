6016e00d19703fc51549792ab1a00636cc16e47d
Cellyane Lima <cellyane@gmail.com>
2025-09-14 10:42:16 +0000

    Add scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # clone-tabnews
+
 Implementation of https://www.tabnews.com.br to practice programming code.
 Tabnews recreation project from scratch taught in course.dev classes
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
-export default function  Home(){
-    return <h1>Marcelo, Eu amo voce, se voce me ama da uma risadinha, hehehe</h1>
+export default function Home() {
+  return <h1>Marcelo, Eu amo voce, se voce me ama da uma risadinha, hehehe</h1>;
 }
